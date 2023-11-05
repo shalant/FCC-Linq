2b1fa2b0bc32cfcc3fb65858921cce075ae89551
shalant <doug.rosenberg@gmail.com>
2023-11-05 10:37:38 -0600

    29:39

--- ThePretendCompayApplication/TCPExtensions/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TCPExtensions
-{
-    public class Class1
-    {
-
-    }
-}
--- ThePretendCompayApplication/ThePretendCompayApplication/Program.cs
+++ ThePretendCompayApplication/ThePretendCompayApplication/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using TCPData;
+using TCPExtensions;
+
+List<Employee> employeeList = Data.GetEmployees();
+var filteredEmployees = employeeList.Filter(emp => emp.IsManager == false);
+
+foreach (var employee in filteredEmployees)
+{
+    Console.WriteLine($"First Name: {employee.FirstName}");
+    Console.WriteLine($"Last Name: {employee.LastName}");
+    Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+    Console.WriteLine($"Manager: {employee.IsManager}");
+    Console.WriteLine();
+}
+
+Console.ReadKey();
